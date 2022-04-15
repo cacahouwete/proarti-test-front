9940700041087d39aae826ceacf7d08d5e81724c
MohaZeggai <mohammedzeggai9@gmail.com>
2022-04-15 18:36:44 +0200

    Chargement données backend

---
+++ components/DataRow.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>{{ firstname }} {{ lastname }} | {{ amount }} | {{ projectName }} | {{ reward }} {{ rewardQuantity }}</p>
+</template>
+
+<script>
+export default {
+  name: 'DataRow',
+
+  props: {
+    'firstname': String,
+    'lastname': String,
+    'amount': Number,
+    'project-name': String,
+    'reward': String,
+    'reward-quantity': Number
+  }
+}
+</script>
+
